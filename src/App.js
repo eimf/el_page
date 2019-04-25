e66bd9c4997eb54f962881919b632ed2df0e2ac8
Ezequiel Lopez <ediaz@elementcorp.com>
2019-04-25 00:38:40 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Sphere from './Sphere'
+import Tree from './Tree'
+
+function App() {
+  return (
+    <div className="App">
+    <Tree className="the-tree"/>
+    <Sphere/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Tree.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import anime from 'animejs'
+import tree from './tree.svg'
+import './tree.css'
+
+function Tree() {
+  return (
+     <img src={tree} className="ugly-tree" />
+  );
+}
+
+export default Tree;
