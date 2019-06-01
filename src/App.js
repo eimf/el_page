cab0a1e33ff52c6f3867333a021157e46bef3b30
Ezequiel <lzdzel@gmail.com>
2019-06-01 19:40:04 -0400

    Add figures into the page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Sphere from './Sphere'
 import Tree from './Tree'
+import Figures from './Figures'
 
 function App() {
   return (
     <div className="App">
     <Tree className="the-tree"/>
     <Sphere/>
+    <Figures/>
     </div>
   );
 }
